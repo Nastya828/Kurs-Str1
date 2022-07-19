3c5db0ef8850eef76a17a3938272f2193a13f137
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-07-19 23:17:26 +0300

    Task03-DynArray

--- src/s3/DynArray.java
+++ src/s3/DynArray.java
@@ -1,45 +1,123 @@
 package s3;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class DynArray<T> {
-    public T [] array;
+    public T[] array;
     public int count;
     public int capacity;
     Class clazz;
 
-    public DynArray(Class clz)
-    {
-        clazz = clz; // нужен для безопасного приведения типов
-        // new DynArray<Integer>(Integer.class);
-
+    public DynArray(Class clz) {
+        clazz = clz;
+        //new DynArray<Integer>(Integer.class);
         count = 0;
         makeArray(16);
     }
 
-    public void makeArray(int new_capacity)
-    {
-        // array = (T[]) Array.newInstance(this.clazz, new_capacity);
-        // ваш код
+    public void makeArray(int new_capacity) {
+        if (new_capacity < 16) {
+            new_capacity = 16;
+        }
+        int n;
+        if (this.array != null) {
+            n = array.length;
+        } else {
+            n = 0;
+        }
+        Object[] arrayCopy = new Object[n];
+        for (int i = 0; i < n; i++) {
+            arrayCopy[i] = this.array[i];
+        }
+        array = (T[]) Array.newInstance(this.clazz, new_capacity);
+        for (int i = 0; i < count - 1; i++) {
+            array[i] = (T) arrayCopy[i];
+        }
     }
 
-    public T getItem(int index)
-    {
-        // ваш код
-        return null;
+    public T getItem(int index) throws ArrayIndexOutOfBoundsException {
+        if (index < array.length) {
+            return array[index];
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 
-    public void append(T itm)
-    {
-        // ваш код
+    public void append(T itm) {
+        if (count >= array.length) {
+            makeArray(array.length * 2);
+        }
+
+        for (int i = 0; i < array.length; i++)
+            if (array[i] == null) {
+                array[i] = itm;
+                this.count++;
+                break;
+            }
     }
 
-    public void insert(T itm, int index)
-    {
-        // ваш код
+
+    public void insert(T itm, int index) throws ArrayIndexOutOfBoundsException {
+        if (index > array.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        count = counter() + 1;
+
+        if (this.count > this.array.length) {
+            makeArray(array.length * 2);
+        }
+        if (count - 1 - index >= 0) {
+            System.arraycopy(array, index, array, index + 1, count - 1 - index);
+        }
+        if (index == array.length) {
+            array[index - 1] = itm;
+        } else {
+            array[index] = itm;
+        }
+
     }
 
-    public void remove(int index)
-    {
-        // ваш код
+
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
+        if (index > array.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if (counter() != 0) {
+            count--;
+        }
+        if (count < (array.length / 2) && array.length != 16) {
+            int x = (int) (array.length / 1.5);
+            makeArray(x);
+        }
+
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = null;
+        if (count > counter()) {
+            count = counter();
+        }
     }
 
+    public int counter() {
+        int y = 0;
+        for (T i : this.array) {
+            if (i != null) {
+                y++;
+            }
+        }
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "DynArray{" +
+                "array=" + Arrays.toString(array) +
+                ", \ncount=" + count +
+                ", array.length=" + array.length +
+                ", clazz=" + clazz +
+                '}';
+    }
 }
--- src/s3/Task03.java
+++ src/s3/Task03.java
@@ -1,7 +1,12 @@
 package s3;
 
+
 public class Task03 {
     public static void main(String[] args) {
+        DynArray<Integer> integerDynArray = new DynArray<>(Integer.class);
+        System.out.println(integerDynArray.counter());
+
 
     }
+
 }
