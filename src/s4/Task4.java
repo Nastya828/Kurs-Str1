ffe07c50bf8b846c0434a2781df4838c5235e1a4
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-07-24 09:01:44 +0300

    brackets

--- src/s4/Task4.java
+++ src/s4/Task4.java
@@ -1,16 +1,13 @@
 package s4;
 
-import java.sql.SQLOutput;
+
 
 public class Task4 {
     public static void main(String[] args) {
-        String str = "8 2 - 5 * 9 + =";
-        System.out.println(funcOrder.order(str));
-        String str1 = "1 2 + 5 * 5 / =";
-        System.out.println(funcOrder.order(str1));
-        String s = ")))";
+
+        String s = "())))()((()";
         System.out.println(functionBr.function(s));
-        System.out.println(functionBr.function("()()"));
+        System.out.println(functionBr.function("()"));
     }
 
 
--- src/s4/functionBr.java
+++ src/s4/functionBr.java
@@ -4,19 +4,17 @@ public class functionBr {
     public static boolean function(String str) {
         Stack<Character> stack = new Stack<>();
         for (char c : str.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else if (stack.size() == 0) {
+            if (stack.size() == 0 && (c != '(')) {
                 return false;
-            }
-            char c1 = stack.pop();
-            if (c1 == '(' && c == ')') {
-
             } else {
+                stack.push(c);
+            }
+        }
+        while (stack.size() > 0) {
+            if (!(stack.pop() == ')' && stack.pop() == '(')) {
                 return false;
             }
         }
-
-        return stack.size() == 0;
+        return true;
     }
 }
